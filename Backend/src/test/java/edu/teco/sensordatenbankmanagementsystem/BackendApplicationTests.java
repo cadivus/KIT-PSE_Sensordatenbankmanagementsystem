8e634afa7cd9249ccac25b34b8a51280e4f3fc19
JaKn <jan.jk.knoblich@gmail.com>
2021-06-07 17:09:42 +0200

    Removed some tests because the application doesn't start as of yet

--- Backend/src/test/java/edu/teco/sensordatenbankmanagementsystem/BackendApplicationTests.java
+++ Backend/src/test/java/edu/teco/sensordatenbankmanagementsystem/BackendApplicationTests.java
@@ -3,10 +3,9 @@ package edu.teco.sensordatenbankmanagementsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
