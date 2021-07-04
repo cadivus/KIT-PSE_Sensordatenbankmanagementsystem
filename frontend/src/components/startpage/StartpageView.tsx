1315ac32706d53f6e0affdb2d0e943efcdc2fa8b
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-04 18:54:09 +0200

    Optische Anpassung der Startseite

--- frontend/src/components/startpage/StartpageView.tsx
+++ frontend/src/components/startpage/StartpageView.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import SensorList from './SensorList'
 import Search from "./Search";
+import {Container, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 /**
  *  Displays the start page.
  *  This class implements a React component.
  */
+
+const useStyles = makeStyles({
+    container: {
+        marginTop: '15px'
+    }
+});
+
 const StartpageView = () => {
-  return (
-    <>
-      <div><b>Startpage</b></div>
-      <p/>
-      <Search />
-      <SensorList />
-    </>
-  )
+    const classes = useStyles()
+    return (
+        <div>
+            <Container maxWidth='lg' className={classes.container}>
+                <Typography variant='h3' align='center' gutterBottom>
+                    Startpage
+                </Typography>
+                <Search/>
+                <SensorList/>
+            </Container>
+        </div>
+    )
 }
 
 export default StartpageView
