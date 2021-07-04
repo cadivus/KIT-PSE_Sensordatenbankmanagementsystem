146360b00365680571cf1f0b787246d03d4f01c0
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-04 18:49:38 +0200

    Optische Anpassung der Sensorliste

--- frontend/src/components/startpage/SensorList.tsx
+++ frontend/src/components/startpage/SensorList.tsx
@@ -1,5 +1,51 @@
 import React from 'react'
 import useSensorStore from '../../hooks/UseSensorStore'
+import {
+    Button,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow, Theme,
+    Typography,
+    withStyles
+} from "@material-ui/core";
+import {createStyles, makeStyles} from "@material-ui/core/styles";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
+const StyledTableCell = withStyles((theme: Theme) =>
+    createStyles({
+        head: {
+            backgroundColor: theme.palette.common.white,
+            color: theme.palette.common.black,
+            fontSize: 17
+        },
+        body: {
+            fontSize: 14,
+        },
+    }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            backgroundColor: theme.palette.common.white,
+        },
+    }),
+)(TableRow);
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 800,
+    },
+    sensorCell: {
+        width: '83%'
+    }
+});
 
 
 /**
@@ -7,15 +53,60 @@ import useSensorStore from '../../hooks/UseSensorStore'
  *  This class implements a React component.
  */
 const SensorList = () => {
+    const classes = useStyles();
     const sensorStore = useSensorStore()
 
     return (
-         <ul>
-             {sensorStore?.sensors.map((reptile) => (
-                 <li>{reptile.name.name}</li>
-             ))}
-         </ul>
-     )
+        <TableContainer component={Paper}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell className={classes.sensorCell}>
+                            <Typography variant='h5'>
+                                <ArrowDropDownIcon/> Sensor
+                            </Typography>
+                        </StyledTableCell>
+                        <StyledTableCell/>
+                        <StyledTableCell/>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {sensorStore?.sensors.map((reptile) => (
+                    <StyledTableRow hover>
+                        <StyledTableCell component="th" scope="row">
+                            <Typography variant='h5'>
+                                {reptile.name.name}
+                            </Typography>
+                        </StyledTableCell>
+                        <StyledTableCell>
+                            <Checkbox
+                                color="primary"
+                                inputProps={{ 'aria-label': 'secondary checkbox' }}
+                            />
+                        </StyledTableCell>
+                        <StyledTableCell>
+                            <Button variant="outlined" color="primary">
+                                    Info
+                            </Button>
+                        </StyledTableCell>
+                    </StyledTableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        /*
+    <ul>
+        {sensorStore?.sensors.map((reptile) => (
+            <li>
+                <Typography variant='h5'>
+                    {reptile.name.name}
+                </Typography>
+            </li>
+        ))}
+    </ul>
+
+         */
+    )
 }
 
 export default SensorList
