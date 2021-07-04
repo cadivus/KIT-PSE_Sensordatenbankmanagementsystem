307f42efe1c0cd6286348c286744fb0cbb08d5c8
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-04 18:49:05 +0200

    Optische Anpassung der Appbar

--- frontend/src/components/layout/AppLayout.tsx
+++ frontend/src/components/layout/AppLayout.tsx
@@ -1,25 +1,33 @@
 import React, {FC} from 'react'
 import {useHistory} from 'react-router-dom'
-import {Button, IconButton, AppBar, Toolbar} from '@material-ui/core'
+import {Button, IconButton, AppBar, Toolbar, Typography} from '@material-ui/core'
 import HomeIcon from '@material-ui/icons/Home'
 
 const AppLayout: FC = ({children}) => {
-  const history = useHistory()
+    const history = useHistory()
 
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => history.push("/")}>
-            <HomeIcon />
-          </IconButton>
-          <Button color="inherit" onClick={() => history.push("/subscriptions")}>Subscriptions</Button>
-          <Button color="inherit" onClick={() => history.push("/login")}>Login</Button>
-        </Toolbar>
-      </AppBar>
-      {children}
-    </>
-  )
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => history.push("/")}>
+                        <HomeIcon/>
+                    </IconButton>
+                    <Button color="inherit" onClick={() => history.push("/subscriptions")}>
+                        <Typography variant='h6'>
+                            Subscriptions
+                        </Typography>
+                    </Button>
+                    <Button color="inherit" onClick={() => history.push("/login")}>
+                        <Typography variant='h6'>
+                            Login
+                        </Typography>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            {children}
+        </>
+    )
 }
 
 export default AppLayout
