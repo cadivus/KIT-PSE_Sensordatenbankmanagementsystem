db2734950d8f28639e1e77d98ad9383140ab40e8
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-11 19:08:27 +0200

    Alle Replayverweise der SensorInformationView leiten nun zur ReplaySingleView weiter

--- frontend/src/components/sensorInformation/SensorInformationView.tsx
+++ frontend/src/components/sensorInformation/SensorInformationView.tsx
@@ -1,9 +1,10 @@
 // http://localhost:3000/sensorInformation
 
-import React from 'react'
+import React, {FC} from 'react'
 import {Button, Container, Grid, Typography} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import {useHistory} from 'react-router-dom'
 import Properties from './Properties'
 import Export from './Export'
 import Data from './Data'
@@ -28,7 +29,8 @@ const useStyles = makeStyles({
  *  Displays the sensor information page.
  *  This class implements a React component.
  */
-const SensorInformationView = () => {
+const SensorInformationView: FC = () => {
+  const history = useHistory()
   const classes = useStyles()
 
   return (
@@ -46,7 +48,11 @@ const SensorInformationView = () => {
           <Grid item xs={6}>
             <Grid container spacing={3}>
               <Grid item xs={12} className={classes.buttonspacing}>
-                <Button variant="outlined" className={classes.button}>
+                <Button
+                  variant="outlined"
+                  className={classes.button}
+                  onClick={() => history.push('/replay/replaySingleView')}
+                >
                   <Typography variant="h5"> Replay </Typography>
                 </Button>
               </Grid>
