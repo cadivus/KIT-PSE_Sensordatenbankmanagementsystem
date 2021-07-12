63e9bf2b029f420eee0f83d603415f217811982e
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-12 15:38:22 +0200

    Subscription Views implementiert

--- frontend/src/components/subscribe/SubscriptionMultipleView.tsx
+++ frontend/src/components/subscribe/SubscriptionMultipleView.tsx
@@ -1,13 +1,96 @@
 // http://localhost:3000/subscriptions/subscriptionMultipleView
 
 import React from 'react'
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Theme,
+  Typography,
+  withStyles,
+} from '@material-ui/core'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
+import Checkbox from '@material-ui/core/Checkbox'
+import {createStyles, makeStyles} from '@material-ui/core/styles'
+import useSensorStore from '../../hooks/UseSensorStore'
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.common.black,
+      fontSize: 17,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }),
+)(TableCell)
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.common.white,
+    },
+  }),
+)(TableRow)
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 800,
+  },
+  sensorCell: {
+    width: '83%',
+  },
+})
 
 /**
  * Displays the webpage of the multiple subscription
  * This class implements a React component.
  */
 const SubscriptionMultipleView = () => {
-  return <div>SubscriptionMultipleView</div>
+  const classes = useStyles()
+  const sensorStore = useSensorStore()
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <StyledTableCell className={classes.sensorCell}>
+              <Typography variant="h5">
+                <ArrowDropDownIcon /> Sensor
+              </Typography>
+            </StyledTableCell>
+            <StyledTableCell />
+            <StyledTableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sensorStore?.sensors.map(sensor => (
+            <StyledTableRow hover key={sensor.name.name}>
+              <StyledTableCell component="th" scope="row">
+                <Typography variant="h5">{sensor.name.name}</Typography>
+              </StyledTableCell>
+              <StyledTableCell>
+                <Checkbox color="primary" inputProps={{'aria-label': 'secondary checkbox'}} />
+              </StyledTableCell>
+              <StyledTableCell>
+                <Button variant="outlined" color="primary">
+                  Info
+                </Button>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
 
 export default SubscriptionMultipleView
