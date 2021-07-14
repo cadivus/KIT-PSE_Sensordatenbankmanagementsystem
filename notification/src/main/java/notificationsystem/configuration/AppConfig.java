7aeb2cf0d59f57f2a2304b97c2755e3f99cb79c8
Jonas Greifenhain <cadivus@daverkomp.de>
2021-07-14 13:20:38 +0200

    Clean up

---
+++ notification/src/main/java/notificationsystem/configuration/AppConfig.java
@@ -0,0 +1,41 @@
+package notificationsystem.configuration;
+
+import notificationsystem.controller.Controller;
+import notificationsystem.controller.MailSender;
+import notificationsystem.model.SensorDAO;
+import notificationsystem.model.SubscriptionDAO;
+import notificationsystem.view.MailBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+//webconfig, restconfig, websecurityconfig, 'webinitializerconfig'
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Controller getMailManager() {
+        return new Controller();
+    }
+
+    @Bean
+    public MailSender getMailSender() {
+        return new MailSender();
+    }
+
+    @Bean
+    public MailBuilder getMailBuilder() {
+        return new MailBuilder();
+    }
+
+    @Bean
+    public SensorDAO getSensorDAO() {
+        return new SensorDAO();
+    }
+
+    @Bean
+    public SubscriptionDAO getSubscriptionDAO() {
+        return new SubscriptionDAO();
+    }
+}
---
+++ notification/src/main/java/notificationsystem/view/Report.java
@@ -0,0 +1,26 @@
+package notificationsystem.view;
+
+import org.json.JSONArray;
+
+import java.util.List;
+
+/**
+ * Report e-mails are sent to subscribers of a sensor at regular intervals dependant on the time of subscription and
+ * user adjustments. The reports contain the data collected by the sensor over the last time period as well as
+ * potentially interesting metadata such as failure rate over the last timer period.
+ * The Report class represents such a report e-mail. It contains the e-mail addresses of sender and receiver, as well
+ * as a subject and a message. Lastly the e-mail includes an attachment with the data collected by the sensor.
+ */
+public class Report extends EMail {
+
+    /**
+     * Constructs a new report e-mail.
+     * @param receiverMail e-mail address the mail is sent to.
+     * @param subject subject of the e-mail.
+     * @param message message body of the e-mail.
+     * @param attachment any data attached to the e-mail.
+     */
+    public Report(String receiverMail, String subject, String message, JSONArray attachment) {
+        super(receiverMail, subject, message, attachment);
+    }
+}
