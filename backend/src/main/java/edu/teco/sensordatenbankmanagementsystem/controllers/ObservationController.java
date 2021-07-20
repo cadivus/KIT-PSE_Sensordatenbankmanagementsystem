c6f835530c148daca7234517ade9271fd739f44b
Jan <jk.jan.knoblich@gmail.com>
2021-07-20 03:25:13 +0200

    adds the structure for the Sensor CSV Export. It is not yet finished, as there isn't a Service method usable for this as of yet

--- backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/controllers/ObservationController.java
+++ backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/controllers/ObservationController.java
@@ -16,7 +16,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -68,4 +76,31 @@ public class ObservationController {
     public List<Observation> getObservationsBySensorId(UUID sensorUUID){
         return null;
     }
+    @GetMapping("/Export/{id}")
+    public void exportToVSC(@PathVariable UUID id, HttpServletResponse response) throws IOException {
+        response.setContentType("text/csv");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=users_" + currentDateTime + ".csv";
+        response.setHeader(headerKey, headerValue);
+
+        //TODO: This is incorrect
+        List<Observation> list = List.of(observationService.getObservation(new UUID(1L,1L)));
+
+        ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.STANDARD_PREFERENCE);
+        String[] csvHeader = {"User ID", "E-mail", "Full Name", "Roles", "Enabled"};
+        String[] nameMapping = {"id", "email", "fullName", "roles", "enabled"};
+
+        csvWriter.writeHeader(csvHeader);
+
+        for (Observation observation : list) {
+            csvWriter.write(observation, nameMapping);
+        }
+
+        csvWriter.close();
+
+    }
+
 }
