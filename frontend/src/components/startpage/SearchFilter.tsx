4ad9b61f1d39de9a838a212d6c04e69058cce2bb
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-21 11:35:51 +0200

    Anpassung der Abstände bei SubscriptionEditor

--- frontend/src/components/startpage/SearchFilter.tsx
+++ frontend/src/components/startpage/SearchFilter.tsx
@@ -4,7 +4,8 @@ import React from 'react'
  *  Represents and implements a filter for sensor lists.
  *  This class implements a React component.
  */
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const SearchFilter = () => {}
+const SearchFilter = () => {
+
+}
 
 export default SearchFilter
