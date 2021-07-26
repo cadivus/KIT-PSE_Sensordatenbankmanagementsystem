928ab4f7cefbb6ccfcada624897f2e36e78c8a1e
Jonas Greifenhain <cadivus@daverkomp.de>
2021-07-27 00:50:52 +0200

    Make backend api url configurable

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,12 @@
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
+const {env} = process
+
 module.exports = function(app) {
   app.use(
     '/api/backend',
     createProxyMiddleware({
-      target: 'http://localhost:8081',
+      target: env.API_BACKEND_URL || 'http://localhost:8081',
       changeOrigin: true,
       pathRewrite: {
         '^/api/backend': '/',
