fd5ed7158f61442e417fcd01b7baa1743c4b2006
timedrix <52511392+timedrix@users.noreply.github.com>
2021-07-28 18:57:36 +0200

    Text der englischen textdatei hinzugefügt

---
+++ frontend/src/intl/map.en.js
@@ -0,0 +1,59 @@
+export default {
+  'en-GB': {
+    // Appbar
+    'appbar.subscription': 'SUBSCRIPTIONS',
+    'appbar.login': 'LOGIN',
+    // Startpage
+    'startpage.startpage': 'Startpage',
+    'startpage.subscribe': 'Subscribe',
+    'startpage.replay': 'Replay',
+    // Infopage
+    'infopage.value': 'Value',
+    'infopage.subscribe': 'Subscribe',
+    'infopage.replay': 'Replay',
+    'infopage.export': 'Export',
+    'infopage.fromDate': 'From date',
+    'infopage.toDate': 'To date',
+    'infopage.fromTime': 'From time',
+    'infopage.toTime': 'To time',
+    'infopage.time': 'Time',
+    'infopage.data': 'Data',
+    // Replaypage
+    'replaypage.speed': 'Speed',
+    'replaypage.play': 'Play',
+    'replaypage.request': 'Request',
+    'replaypage.help': 'Help',
+    'replaypage.helpTextHedding': 'Need Help?',
+    'replaypage.helpText':
+      'Lorem ipsum dolor sit amet, ' +
+      'consetetur sadipscing elitr, sed diam nonumy eirmod tempor ' +
+      'invidunt ut labore et et dolore magna aliquyam erat, sed diam ' +
+      'voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ' +
+      'clita kasd gubergren, no sea takimata sanctus est Lorem ipsum ' +
+      'dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing ' +
+      'elitr, sed diam nonumy eirmod tempor invidunt ut labore et ' +
+      'dolore magna aliquyam erat, sed diam voluptua. At vero eos et ' +
+      'accusam et justo duo dolores et ea rebum. Stet clita kasd ' +
+      'gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    'replaypage.sensor': 'Sensor',
+    'replaypage.value': 'Value',
+    // Subsciptionpage
+    'subscriptionpage.notificationMessage': 'Direct notification on failures',
+    'subscriptionpage.protocolMessage1': 'Protocol every',
+    'subscriptionpage.protocolMessage2': 'days',
+    'subscriptionpage.sensor': 'Sensor',
+    // Subscriptionspage
+    'subscriptionspage.logLevel': 'Log level',
+    'subscriptionspage.notificationError': 'Notification on Error',
+    'subscriptionspage.unsubscribe': 'Unsubscribe',
+    'subscriptionspage.changeButton': 'CHANGE',
+    'subscriptionspage.deleteButton': 'DELETE',
+    'subscriptionspage.logMessage1': 'Every',
+    'subscriptionspage.logMessage2': 'days',
+    // Login
+    'loginpage.signIn': 'Sign in',
+    'loginpage.signInButton': 'SIGN IN',
+    'loginpage.emailaddress': 'Email Address *',
+    'loginpage.password': 'Password *',
+  },
+}
