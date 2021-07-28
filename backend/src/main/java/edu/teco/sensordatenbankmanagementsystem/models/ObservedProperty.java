1895e0f9b66f2ae97fd8e3469f4426d52f2cef6b
JaKn <jan.jk.knoblich@gmail.com>
2021-07-28 07:04:33 +0200

    Implement ObserverProperty

--- backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/models/ObservedProperty.java
+++ backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/models/ObservedProperty.java
@@ -1,5 +1,6 @@
 package edu.teco.sensordatenbankmanagementsystem.models;
 
+import javax.persistence.Table;
 import lombok.Data;
 
 import javax.persistence.Column;
@@ -8,10 +9,23 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@Table(name = "\"OBS_PROPERTIES\"")
 public class ObservedProperty {
 
     @Column(name = "\"ID\"")
     @Id
     String id;
+
+    @Column(name = "\"NAME\"")
+    String name;
+
+    @Column(name = "\"DEFINITION\"")
+    String definition;
+
+    @Column(name = "\"DESCRIPTION\"")
+    String description;
+
+    @Column(name = "\"PROPERTIES\"")
+    String properties;
 
 }
