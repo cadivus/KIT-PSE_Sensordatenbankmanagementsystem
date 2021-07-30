2dde0e962afebaa83d84a169dd9f13bedec38b0f
Jonas Greifenhain <cadivus@daverkomp.de>
2021-07-30 16:13:58 +0200

    Introduce sensor properties

--- frontend/src/material/Sensor.ts
+++ frontend/src/material/Sensor.ts
@@ -1,6 +1,7 @@
 import SensorValue from './SensorValue'
 import SensorName from './SensorName'
 import Id from './Id'
+import SensorProperty from './SensorProperty'
 
 /**
  * this class represents a sensor.
@@ -16,9 +17,31 @@ abstract class Sensor {
    */
   readonly id: Id
 
+  private _properties: Map<string, SensorProperty>
+
   constructor(name: SensorName, id: Id) {
+    this._properties = new Map<string, SensorProperty>()
     this.name = name
     this.id = id
+  }
+
+  get properties(): Array<SensorProperty> {
+    const {_properties} = this
+    const result = new Array<SensorProperty>()
+
+    _properties.forEach(e => {
+      result.push(e)
+    })
+
+    return result
+  }
+
+  setProperty = (property: SensorProperty): void => {
+    const {_properties} = this
+    const oldProperty = _properties.get(property.key)
+    if (oldProperty && oldProperty.value === property.value) return
+
+    _properties.set(property.key, property)
   }
 
   /**
---
+++ frontend/src/material/SensorProperty.ts
@@ -0,0 +1,12 @@
+class SensorProperty {
+  readonly key: string
+
+  readonly value: string
+
+  constructor(key: string, value: string) {
+    this.key = key
+    this.value = value
+  }
+}
+
+export default SensorProperty
