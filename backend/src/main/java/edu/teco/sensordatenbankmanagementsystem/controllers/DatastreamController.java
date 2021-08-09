b74e0cbd448f39e744769176d691089f252f6929
JaKn <jan.jk.knoblich@gmail.com>
2021-08-09 15:13:34 +0200

    Creates datastream controller
    Creates a datastream controller and implements two different ways to export information about them
    
    They both list the datastreams from a specific thing. One as a csv and the other as a JSON

---
+++ backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/controllers/DatastreamController.java
@@ -0,0 +1,68 @@
+package edu.teco.sensordatenbankmanagementsystem.controllers;
+
+import edu.teco.sensordatenbankmanagementsystem.models.Datastream;
+import edu.teco.sensordatenbankmanagementsystem.models.Observation;
+import edu.teco.sensordatenbankmanagementsystem.services.ObservationService;
+import edu.teco.sensordatenbankmanagementsystem.services.ObservationServiceImp;
+import edu.teco.sensordatenbankmanagementsystem.services.SensorServiceImp;
+import edu.teco.sensordatenbankmanagementsystem.services.ThingService;
+import edu.teco.sensordatenbankmanagementsystem.services.ThingServiceImp;
+import edu.teco.sensordatenbankmanagementsystem.util.WriteCsvToResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/datastream")
+public class DatastreamController {
+
+  private final ThingService thingService;
+  private final ObservationService observationService;
+
+  public DatastreamController(
+      ThingServiceImp thingService, SensorServiceImp sensorService,
+      ObservationServiceImp observationService) {
+    this.thingService = thingService;
+    this.observationService = observationService;
+  }
+
+  @GetMapping("/listDatastreams")
+  @Transactional
+  public List<Datastream> getDatastreams(@RequestParam(value = "id") String thingId){
+    return thingService.getThing(thingId).getDatastream();
+  }
+
+  @GetMapping("/export")
+  @Transactional
+  public void exportToCsv(@RequestParam(value =  "id") String datastreamId,@RequestParam(value = "limit", required = false) int limit, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/csv");
+    DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:HH-mm-ss");
+    String currentDateTime = dateFormatter.format(new Date());
+
+    if (limit == 0){
+      limit = Integer.MAX_VALUE;
+    }
+    String headerKey = "Content-Disposition";
+    String headerValue = "attachment; filename=users_" + currentDateTime + ".csv";
+    response.setHeader(headerKey, headerValue);
+    Stream<Observation> list = observationService.getObservationByDatastreamId(datastreamId,
+        PageRequest.of(0,limit));
+
+    WriteCsvToResponse.writeObservation(response.getWriter(), list);
+
+
+  }
+
+
+}
