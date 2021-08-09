6ac9ee5304a9d86bf04a3295c8e37094fe9785f3
Jonas Greifenhain <cadivus@daverkomp.de>
2021-08-09 12:38:12 +0200

    New backend functions

--- frontend/src/store/communication/backendUrlCreator.ts
+++ frontend/src/store/communication/backendUrlCreator.ts
@@ -5,3 +5,9 @@ export const BACKEND_PATH = `${window.location.protocol}//${window.location.host
 export const ALL_THINGS = `${BACKEND_PATH}/sensor/allThings`
 
 export const getActiveStateUrl = (sensorId: Id): string => `${BACKEND_PATH}/sensor/active?ids=${sensorId.toString()}`
+
+export const getAllDatastreamsUrl = (sensorId: Id): string =>
+  `${BACKEND_PATH}/datastream/listDatastreams?id=${sensorId.toString()}`
+
+export const getExportDatastreamUrl = (datastreamId: Id): string =>
+  `${BACKEND_PATH}/datastream/export?id=${datastreamId.toString()}`
