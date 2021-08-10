d10d62cfe29e3257c251f19852ac7f4d4a9de937
timedrix <52511392+timedrix@users.noreply.github.com>
2021-08-10 18:08:07 +0200

    Fixes Arrowfunction in cloned data

--- frontend/src/store/communication/notificationUrlCreator.ts
+++ frontend/src/store/communication/notificationUrlCreator.ts
@@ -1,1 +1,9 @@
 export const NOTIFICATION_PATH = `${window.location.protocol}//${window.location.host}/api/notification`
+
+export const CONFIRMCODE_PATH = `${NOTIFICATION_PATH}/getConfirmCode`
+
+export const POST_SUBSCRIPTION_PATH = `${NOTIFICATION_PATH}/postSubscription`
+
+export const POST_UBSUBSCRIBE_PATH = `${NOTIFICATION_PATH}/postUnsubscribe`
+
+export const GET_SUBSCRIPTION_PATH = `${NOTIFICATION_PATH}/getSubscriptions`
