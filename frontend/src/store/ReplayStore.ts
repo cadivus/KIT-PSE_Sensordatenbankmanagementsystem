ab6e927eb0359f8b575ca53eb828bc169f506819
Jonas Greifenhain <cadivus@daverkomp.de>
2021-08-11 13:48:42 +0200

    Create replay

--- frontend/src/store/ReplayStore.ts
+++ frontend/src/store/ReplayStore.ts
@@ -3,6 +3,8 @@ import Thing from '../material/Thing'
 import ReplaySpeed from '../material/ReplaySpeed'
 import {CREATE_REPLAY} from './communication/backendUrlCreator'
 import {createReplayRequestJson} from './communication/backendJsonCreator'
+import {postJsonGetText} from './communication/restClient'
+import Id from '../material/Id'
 
 /**
  * This is the storage for replays.
@@ -23,7 +25,13 @@ class ReplayStore {
     const json = createReplayRequestJson(start, end, speed, things)
 
     const resultPromise = new Promise<Replay | null>((resolve, reject) => {
-      resolve(null)
+      postJsonGetText(url, json).then((newIdString: string) => {
+        const newId = new Id(newIdString)
+        console.log('newId', newId)
+        const newReplay = new Replay(newId, start, end, speed, things)
+
+        resolve(newReplay)
+      })
     })
 
     return resultPromise
