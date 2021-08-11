7deaec68a6143588e27a206e0293051ece752a28
Jonas Greifenhain <cadivus@daverkomp.de>
2021-08-11 02:54:37 +0200

    Frontend for downloading CSV
    backendUrlCreator.ts needs to be adjusted for that

--- frontend/src/store/communication/backendUrlCreator.ts
+++ frontend/src/store/communication/backendUrlCreator.ts
@@ -1,5 +1,10 @@
 import Id from '../../material/Id'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dateFormat = require('dateformat')
+
+const toBackendDate = (date: Date) => dateFormat(date, 'yyyy-mm-dd:HH-MM-ss')
+
 export const BACKEND_PATH = `${window.location.protocol}//${window.location.host}/api/backend`
 
 export const ALL_THINGS = `${BACKEND_PATH}/sensor/allThings`
@@ -11,3 +16,11 @@ export const getAllThingDatastreamsUrl = (thingId: Id): string =>
 
 export const getExportDatastreamUrl = (datastreamId: Id, limit: number): string =>
   `${BACKEND_PATH}/datastream/export?id=${datastreamId.toString()}${limit < 1 ? '' : `&limit=${limit}`}`
+
+export const getCsvDownloadUrl = (datastreamId: Id, startDate: Date, endDate: Date): string => {
+  const formatedStart = `startDate=${toBackendDate(startDate)}`
+  const formatedEnd = `endDate=${toBackendDate(endDate)}`
+
+  // return `${BACKEND_PATH}/datastream/export?id=${datastreamId.toString()}&${formatedStart}&${formatedEnd}`
+  return `https://www.teco.edu/wp-content/themes/teco/images/teco_trans.png?${formatedStart}&${formatedEnd}`
+}
