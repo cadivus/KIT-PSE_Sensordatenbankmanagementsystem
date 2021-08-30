834c510f9de50119fc47b961d8923230bec56c8a
Jonas Greifenhain <cadivus@daverkomp.de>
2021-08-31 01:35:45 +0200

    Add mock data for email login

--- frontend/src/test/mock/store/communication/mockData/backend/getText.js
+++ frontend/src/test/mock/store/communication/mockData/backend/getText.js
@@ -3,12 +3,23 @@ import {getExportDatastreamUrl} from '../../../../../../store/communication/back
 import Id from '../../../../../../material/Id'
 import {datastreamSensor1Id, datastreamSensor2Id, datastreamSensor3Id} from './getJson'
 import {sensor1Datastream1Csv, sensor2Datastream1Csv, sensor3Datastream1Csv} from './files/csvFiles'
+import EMail from '../../../../../../material/EMail'
+import {getConfirmCodeUrl} from '../../../../../../store/communication/notificationUrlCreator'
 /* eslint-enable */
+
+export const email1 = new EMail('test1@test.tld')
+export const email2 = new EMail('test2@test2.tttld')
+
+export const confirm1 = 'TbzVFyap'
+export const confirm2 = 'XcQRZTqP'
 
 export const getTextMap = new Map()
 
 getTextMap.set(getExportDatastreamUrl(new Id(datastreamSensor1Id), 25), sensor1Datastream1Csv)
 getTextMap.set(getExportDatastreamUrl(new Id(datastreamSensor2Id), 25), sensor2Datastream1Csv)
 getTextMap.set(getExportDatastreamUrl(new Id(datastreamSensor3Id), 25), sensor3Datastream1Csv)
+
+getTextMap.set(getConfirmCodeUrl(email1), confirm1)
+getTextMap.set(getConfirmCodeUrl(email2), confirm2)
 
 export default getTextMap
