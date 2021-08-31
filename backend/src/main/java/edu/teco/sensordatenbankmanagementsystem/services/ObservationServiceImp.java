7ad10100a4208c6f4df639fa1013b1ecc0df42e5
ff781 <spma2@gmx.net>
2021-08-31 18:13:23 +0200

    added mocked graph drawing, but doesn't work as intended yet

--- backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/services/ObservationServiceImp.java
+++ backend/src/main/java/edu/teco/sensordatenbankmanagementsystem/services/ObservationServiceImp.java
@@ -84,7 +84,7 @@ public class ObservationServiceImp implements ObservationService{
     LocalDateTime finalFrameEnd = frameEnd;
     return associatedStreams.stream()
             .flatMap(a-> this.observationRepository
-                    .findObservationsByDatastreamIdAndPhenomenonStartAfterAndPhenomenonEndBeforeOrderByPhenomenonStartAsc(a.getId(),
+                    .findObservationsByDatastreamIdAndPhenomenonStartAfterAndPhenomenonEndBeforeOrderByPhenomenonStartDesc(a.getId(),
                             finalFrameStart, finalFrameEnd, PageRequest.of(0, limit).withSort(sort)))
             .limit(limit)
             .collect(Collectors.toList());
--- backend/src/test/java/edu/teco/sensordatenbankmanagementsystem/services/GraphServiceImpTest.java
+++ backend/src/test/java/edu/teco/sensordatenbankmanagementsystem/services/GraphServiceImpTest.java
@@ -1,11 +1,15 @@
 package edu.teco.sensordatenbankmanagementsystem.services;
 
 import edu.teco.sensordatenbankmanagementsystem.exceptions.CantInterpolateWithNoSamplesException;
+import edu.teco.sensordatenbankmanagementsystem.models.Datastream;
+import edu.teco.sensordatenbankmanagementsystem.models.Observation;
 import edu.teco.sensordatenbankmanagementsystem.repository.DatastreamRepository;
 import edu.teco.sensordatenbankmanagementsystem.repository.ObservationRepository;
 import edu.teco.sensordatenbankmanagementsystem.repository.ObservedPropertyRepository;
 import edu.teco.sensordatenbankmanagementsystem.util.interpolation.LagrangeInterpolator;
+import edu.teco.sensordatenbankmanagementsystem.util.interpolation.NewtonInterpolator;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -19,51 +23,131 @@ import org.springframework.context.annotation.Import;
 
 import javax.persistence.EntityManager;
 import java.awt.*;
+import java.awt.image.RenderedImage;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 
 @DataJpaTest
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 class GraphServiceImpTest {
 
-  @Autowired
-  GraphServiceImp graphServiceImp;
-
-  @MockBean
-  private ObservationService observationService;
-  @MockBean
-  private DatastreamRepository datastreamRepository;
-
-  @Test
-  void getGraphImageOfThingNoInterpolationPoints() {
-    assertThrows(CantInterpolateWithNoSamplesException.class, () -> {
-      graphServiceImp.getGraphImageOfThing(
-              "saqn:t:00afbb4",
-              "obsId=saqn:op:hur",
-              null, null,
-              0,
-              new Dimension(69, 69),
-              3,
-              LagrangeInterpolator.getInstance()
-      );
-    });
-  }
-
-  @Configuration
-  @Import(GraphServiceImp.class)
-  static class TestConfig {
     @Autowired
-    EntityManager entityManager;
+    GraphServiceImp graphServiceImp;
 
-    @Bean
-    ObservationService observationService() {
-      return mock(ObservationService.class);
-    }
-    @Bean
-    DatastreamRepository datastreamRepository() {
-      return mock(DatastreamRepository.class);
+    @MockBean
+    private ObservationService observationService;
+    @MockBean
+    private ObservationRepository observationRepository;
+    @MockBean
+    private DatastreamRepository datastreamRepository;
+
+    @Test
+    void getGraphImageOfThingNoInterpolationPoints() {
+        assertThrows(CantInterpolateWithNoSamplesException.class, () -> {
+            graphServiceImp.getGraphImageOfThing(
+                    "a",
+                    "b",
+//                    "saqn:t:00afbb4",
+//                    "obsId=saqn:op:hur",
+                    null, null,
+                    0,
+                    new Dimension(69, 69),
+                    3,
+                    LagrangeInterpolator.getInstance()
+            );
+        });
     }
 
-  }
+    @Test
+    void getGraphImageOfInvalidThing() {
+        assertThrows(CantInterpolateWithNoSamplesException.class, () -> {
+            graphServiceImp.getGraphImageOfThing(
+                    "xxxxxxx",
+                    "ddddddd",
+                    null, null,
+                    10,
+                    new Dimension(69, 69),
+                    7,
+                    LagrangeInterpolator.getInstance()
+            );
+        });
+    }
+
+    @Test
+    void getGraphImageOfThing() {
+        Dimension dim = new Dimension(69, 69);
+        RenderedImage graphImage = graphServiceImp.getGraphImageOfThing(
+                "a",
+                "b",
+//                    "saqn:t:00afbb4",
+//                    "obsId=saqn:op:hur",
+                null, null,
+                5,
+                dim,
+                3,
+                LagrangeInterpolator.getInstance()
+        );
+
+        assertEquals(dim.getWidth(), graphImage.getWidth());
+        assertEquals(dim.getHeight(), graphImage.getHeight());
+    }
+
+    @Configuration
+    @Import(GraphServiceImp.class)
+    static class TestConfig {
+        @Autowired
+        EntityManager entityManager;
+
+        int intervalDayWidth = 50;
+        int obsN = 5;
+        Datastream mr = new Datastream();
+        List<Observation> obs = new ArrayList<>();
+        LocalDateTime now = LocalDateTime.now();
+        {
+            mr.setId("c");
+            mr.setObsId("b");
+            mr.setPhenomenonStart(now.minusDays(intervalDayWidth));
+            mr.setPhenomenonEnd(now);
+
+            for(int i=0; i<obsN; i++){
+                Observation toAdd = new Observation();
+                toAdd.setResultTime(now.minusDays(intervalDayWidth).plusDays((long) i * intervalDayWidth / obsN));
+                toAdd.setResultNumber((double)i*i);
+                obs.add(toAdd);
+            }
+        }
+
+        @Bean
+        ObservationService observationService() {
+            ObservationService observationService = mock(ObservationService.class);
+            return observationService;
+        }
+
+        @Bean
+        ObservationRepository observationRepository() {
+            ObservationRepository observationRepository = mock(ObservationRepository.class);
+            Mockito.when(observationRepository.findObservationsByDatastreamIdAndPhenomenonStartAfterAndPhenomenonEndBeforeOrderByPhenomenonStartDesc(
+                    eq(mr.getId()),
+                    any(), any(),
+                    any()
+            )).thenReturn(obs.stream());
+            return observationRepository;
+        }
+
+        @Bean
+        DatastreamRepository datastreamRepository() {
+            DatastreamRepository datastreamRepository = mock(DatastreamRepository.class);
+            Mockito.when(datastreamRepository.findDatastreamsByThing_IdAndObsIdIn("a", List.of(mr.getObsId()))).thenReturn(List.of(mr));
+            return datastreamRepository;
+        }
+
+    }
 }
