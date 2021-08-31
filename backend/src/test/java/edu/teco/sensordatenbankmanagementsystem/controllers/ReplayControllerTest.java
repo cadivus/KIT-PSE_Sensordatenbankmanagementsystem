4b1baed510bc298f9ca0cb39b22338d659063515
JaKn <jan.jk.knoblich@gmail.com>
2021-08-31 09:28:13 +0200

    Add unfinished replay test

--- backend/src/test/java/edu/teco/sensordatenbankmanagementsystem/controllers/ReplayControllerTest.java
+++ backend/src/test/java/edu/teco/sensordatenbankmanagementsystem/controllers/ReplayControllerTest.java
@@ -2,8 +2,11 @@ package edu.teco.sensordatenbankmanagementsystem.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM;
 
+import java.util.function.Consumer;
 import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -18,10 +21,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 @ComponentScan(basePackages = {"edu.teco.sensordatenbamkmanagementsystem"})
+@TestPropertySource(properties = "spring.mvc.async.request-timeout= -1")
 class ReplayControllerTest {
 
   @Autowired
@@ -105,7 +111,46 @@ class ReplayControllerTest {
   }
 
   @Test
+  @Disabled
   void streamSseMvc() {
+    String[] array = {"saqn:t:43ae704",
+        "saqn:t:grimm-aerosol.com:EDM80NEPH:SN17001",
+        "saqn:t:4049564"};
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("start", "25/11/2019 01:01:00");
+    jsonObject.put("end", "28/11/2019 01:01:03");
+    jsonObject.put("speed", 50000);
+    jsonObject.put("sensors", array);
+    HttpEntity<String> entity = new HttpEntity<String>(jsonObject.toJSONString(), headers);
+
+    ResponseEntity<String> response = testRestTemplate.exchange(
+        createURLWithPort("/observation/newSSE"), HttpMethod.POST, entity,
+        String.class);
+    var id = response.getBody();
+    entity = new HttpEntity<>(null,headers);
+    response = testRestTemplate.exchange(
+        createURLWithPort("/observation/stream/"+id), HttpMethod.GET, entity,
+        String.class);
+
+    WebTestClient client = WebTestClient.bindToServer()
+        .baseUrl("http://localhost:"+port)
+        .build();
+
+    client.get()
+        .uri("/sse/"+id)
+        .accept(TEXT_EVENT_STREAM)
+        .exchange()
+        .expectStatus().isOk()
+        .returnResult(String.class)
+        .getResponseBody()
+        .subscribe(new Consumer<String>() {
+          @Override
+          public void accept(String event) {
+            assert(true);
+          }
+        });
+
   }
 
   private String createURLWithPort(String uri) {
