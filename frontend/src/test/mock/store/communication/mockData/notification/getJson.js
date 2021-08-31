6523f0fb703e3f58fefbeae8cecf7fe158ad38d9
Jonas Greifenhain <cadivus@daverkomp.de>
2021-08-31 15:03:11 +0200

    Fix mock data

--- frontend/src/test/mock/store/communication/mockData/notification/getJson.js
+++ frontend/src/test/mock/store/communication/mockData/notification/getJson.js
@@ -1,14 +1,13 @@
 import {email1} from './getText'
 import {sensor1Id, sensor2Id} from '../backend/getJson'
 import {getSubscriptionsUrl} from '../../../../../../store/communication/notificationUrlCreator'
-import EMail from '../../../../../../material/EMail'
 
-export const user1SubscriptionsFirst = [{"id":19,"subscriberAddress":email1,"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":true}]
-export const user1SubscriptionsSecond = [{"id":19,"subscriberAddress":email1,"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":true},
-  {"id":20,"subscriberAddress":email1,"sensorId":sensor2Id,"subTime":"2021-08-31","reportInterval":7,"toggleAlert":false}]
+export const user1SubscriptionsFirst = [{"id":19,"subscriberAddress":email1.toString(),"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":true}]
+export const user1SubscriptionsSecond = [{"id":19,"subscriberAddress":email1.toString(),"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":true},
+  {"id":20,"subscriberAddress":email1.toString(),"sensorId":sensor2Id,"subTime":"2021-08-31","reportInterval":7,"toggleAlert":false}]
 
 export const getJsonMap = new Map()
 
-getJsonMap.set(getSubscriptionsUrl(new EMail(email1)), user1SubscriptionsFirst)
+getJsonMap.set(getSubscriptionsUrl(email1), user1SubscriptionsFirst)
 
 export default getJsonMap
