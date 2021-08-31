24db6fe039a0e30cac71717086069d7d37f10375
Jonas Greifenhain <cadivus@daverkomp.de>
2021-08-31 11:57:42 +0200

    Fix mock data

--- frontend/src/test/mock/store/communication/mockData/notification/getJson.js
+++ frontend/src/test/mock/store/communication/mockData/notification/getJson.js
@@ -3,7 +3,7 @@ import {sensor1Id, sensor2Id} from '../backend/getJson'
 import {getSubscriptionsUrl} from '../../../../../../store/communication/notificationUrlCreator'
 import EMail from '../../../../../../material/EMail'
 
-export const user1SubscriptionsFirst = [{"id":19,"subscriberAddress":email1,"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":false}]
+export const user1SubscriptionsFirst = [{"id":19,"subscriberAddress":email1,"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":true}]
 export const user1SubscriptionsSecond = [{"id":19,"subscriberAddress":email1,"sensorId":sensor1Id,"subTime":"2021-08-31","reportInterval":6,"toggleAlert":true},
   {"id":20,"subscriberAddress":email1,"sensorId":sensor2Id,"subTime":"2021-08-31","reportInterval":7,"toggleAlert":false}]
 
