71c11a8956d2b6f58c2a1b756e12f991aa40e253
timedrix <52511392+timedrix@users.noreply.github.com>
2021-09-05 23:38:56 +0200

    Fix ReplayView test by mocking props and Redirect

--- frontend/src/components/replay/ReplayView.test.js
+++ frontend/src/components/replay/ReplayView.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import reactRouterDom from 'react-router-dom'
 import {getJson, getText, postJsonGetText} from '../../store/communication/restClient'
 import {
   getJson as getJsonMock,
@@ -8,8 +9,24 @@ import {
 import ReplayView from './ReplayView'
 import {renderWithProviders} from '../../test/jestHelper/customRender'
 import {sensor1, sensor2, sensor3} from '../../test/mock/store/communication/mockData/backend/getJson'
+import ThingStore from '../../store/ThingStore'
+import DatastreamStore from '../../store/DatastreamStore'
 
 jest.mock('../../store/communication/restClient')
+jest.mock('react-router-dom')
+
+const getLocation = (selectedSensors = []) => {
+  const location = {
+    pathname: '/subscriptions/subscriptionCreate',
+    state: {
+      selectedThings: new Set(selectedSensors),
+    },
+    search: '',
+    hash: '',
+    key: 'r6c784',
+  }
+  return location
+}
 
 beforeEach(() => {
   getJson.mockImplementation(getJsonMock)
@@ -18,8 +35,12 @@ beforeEach(() => {
 })
 
 test('check for elements', async () => {
+  const thingStore = new ThingStore(new DatastreamStore())
+  const location = getLocation(await thingStore.things)
+
+  reactRouterDom.useParams = jest.fn().mockReturnValue({})
   const selectedThings = {sensor1, sensor2, sensor3}
-  const {getByTestId} = renderWithProviders(<ReplayView state={selectedThings} />)
+  const {getByTestId} = renderWithProviders(<ReplayView location={location} />)
 
   const replayViewContainer = getByTestId(/replayView-container/)
   expect(replayViewContainer).toBeInTheDocument()
