7ab6e8e8f16524a2b9015ba9966e2cd68502d483
Jonas Greifenhain <cadivus@daverkomp.de>
2021-09-05 22:09:42 +0200

    Fix test by mocking props and Redirect

--- frontend/src/components/subscribe/SubscriptionEditorView.test.js
+++ frontend/src/components/subscribe/SubscriptionEditorView.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import reactRouterDom from 'react-router-dom'
 import {getJson, getText, postJsonGetText} from '../../store/communication/restClient'
 import {
   getJson as getJsonMock,
@@ -7,17 +8,41 @@ import {
 } from '../../test/mock/store/communication/restClientMock'
 import {renderWithProviders} from '../../test/jestHelper/customRender'
 import SubscriptionEditorView from './SubscriptionEditorView'
+import DatastreamStore from '../../store/DatastreamStore'
+import ThingStore from '../../store/ThingStore'
 
 jest.mock('../../store/communication/restClient')
+jest.mock('react-router-dom')
+
+const getLocation = (selectedSensors = []) => {
+  const location = {
+    pathname: '/subscriptions/subscriptionCreate',
+    state: {
+      selectedThings: new Set(selectedSensors),
+    },
+    search: '',
+    hash: '',
+    key: 'r6c784',
+  }
+  return location
+}
 
 beforeEach(() => {
   getJson.mockImplementation(getJsonMock)
   getText.mockImplementation(getTextMock)
   postJsonGetText.mockImplementation(postJsonGetTextMock)
+
+  const pushMock = jest.fn()
+  reactRouterDom.useHistory = jest.fn().mockReturnValue({push: pushMock})
+  reactRouterDom.Redirect = jest.fn().mockReturnValue(<div data-testid="replaces-redirect" />)
 })
 
-test('check for elements', async () => {
-  const {getByTestId} = renderWithProviders(<SubscriptionEditorView />)
+test('check for elements on creating a new subscription with multiple things', async () => {
+  const thingStore = new ThingStore(new DatastreamStore())
+  const location = getLocation(await thingStore.things)
+
+  reactRouterDom.useParams = jest.fn().mockReturnValue({})
+  const {getByTestId} = renderWithProviders(<SubscriptionEditorView location={location} />)
 
   const subscribeChangeButton = getByTestId(/subscribe-change-button/)
   const thingTable = getByTestId(/thing-table/)
