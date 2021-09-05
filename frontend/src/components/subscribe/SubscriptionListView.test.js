07890ef98d3af83e36dfe486cde752e93dbb37e0
timedrix <52511392+timedrix@users.noreply.github.com>
2021-09-05 23:45:49 +0200

    Commitfehler behoben (SubscriptionListView)

--- frontend/src/components/subscribe/SubscriptionListView.test.js
+++ frontend/src/components/subscribe/SubscriptionListView.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import reactRouterDom from 'react-router-dom'
 import {getJson, getText, postJsonGetText} from '../../store/communication/restClient'
 import {
   getJson as getJsonMock,
@@ -7,45 +6,50 @@ import {
   postJsonGetText as postJsonGetTextMock,
 } from '../../test/mock/store/communication/restClientMock'
 import {renderWithProviders} from '../../test/jestHelper/customRender'
-import SubscriptionEditorView from './SubscriptionEditorView'
-import DatastreamStore from '../../store/DatastreamStore'
-import ThingStore from '../../store/ThingStore'
+import SubscriptionListView from './SubscriptionListView'
 
 jest.mock('../../store/communication/restClient')
-jest.mock('react-router-dom')
-
-const getLocation = (selectedSensors = []) => {
-  const location = {
-    pathname: '/subscriptions/subscriptionCreate',
-    state: {
-      selectedThings: new Set(selectedSensors),
-    },
-    search: '',
-    hash: '',
-    key: 'r6c784',
-  }
-  return location
-}
 
 beforeEach(() => {
   getJson.mockImplementation(getJsonMock)
   getText.mockImplementation(getTextMock)
   postJsonGetText.mockImplementation(postJsonGetTextMock)
-
-  const pushMock = jest.fn()
-  reactRouterDom.useHistory = jest.fn().mockReturnValue({push: pushMock})
-  reactRouterDom.Redirect = jest.fn().mockReturnValue(<div data-testid="replaces-redirect" />)
 })
 
-test('check for elements on creating a new subscription with multiple things', async () => {
-  const thingStore = new ThingStore(new DatastreamStore())
-  const location = getLocation(await thingStore.things)
+test('check for elements', async () => {
+  const props = {
+    history: {
+      length: 4,
+      action: 'PUSH',
+      location: {
+        pathname: '/subscriptions/subscriptionCreate',
+        state: {
+          selectedThings: {},
+        },
+        search: '',
+        hash: '',
+        key: 'r6c784',
+      },
+    },
+    location: {
+      pathname: '/subscriptions/subscriptionCreate',
+      state: {
+        selectedThings: {},
+      },
+      search: '',
+      hash: '',
+      key: 'r6c784',
+    },
+    match: {
+      path: '/subscriptions/subscriptionCreate',
+      url: '/subscriptions/subscriptionCreate',
+      isExact: true,
+      params: {},
+    },
+  }
 
-  reactRouterDom.useParams = jest.fn().mockReturnValue({})
-  const {getByTestId} = renderWithProviders(<SubscriptionEditorView location={location} />)
+  const {getByTestId} = renderWithProviders(<SubscriptionListView props={props} />)
 
-  const subscribeChangeButton = getByTestId(/subscribe-change-button/)
-  const thingTable = getByTestId(/thing-table/)
-  expect(subscribeChangeButton).toBeInTheDocument()
-  expect(thingTable).toBeInTheDocument()
+  const subscriptionListViewContainer = getByTestId(/subscriptionListView-Container/)
+  expect(subscriptionListViewContainer).toBeInTheDocument()
 })
